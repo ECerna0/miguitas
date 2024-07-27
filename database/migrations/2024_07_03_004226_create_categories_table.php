91d4e2783a38559bcc40562e390f4721016b95c0
wom987 <wom987@hotmail.com>
2024-07-26 18:28:51 -0600

    feat: update db and start validations

--- database/migrations/2024_07_03_004226_create_categories_table.php
+++ database/migrations/2024_07_03_004226_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string("name", 50);
+            $table->integer("parent_id", 50)->nullable();
             $table->string("image", 200)->nullable();
             $table->timestamps();
         });
