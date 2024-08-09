d68faf42f5a259ccf9399ac5750a9c624480d6ce
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-08 21:51:52 -0600

    feat: add section pawty packs

---
+++ resources/js/Components/frontend/Home/PawtyPacks.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="mx-auto mt-4 p-10 w-4/5">
+        <div class="flex flex-col items-center">
+            <h3 class="font-pluto_m text-4xl text-blue">PAWTY PACKS</h3>
+            <p
+                class="mx-auto mt-2 w-4/5 font-dine_r font-medium text-center text-gray"
+            >
+                ¡La mejor manera de celebrar a tu peludo en su cumpleaños, su
+                adopción, su logro o cualquier otro motivo! Incluye pastel o
+                pupcakes, 1 gorro, 3 globos, 1 galleta, 1 número de galleta y 1
+                bandana birthday boy o girl. También puedes comprar estos
+                artículos individualmente.
+            </p>
+        </div>
+        <a href="/shop" class="font-pluto_m text-2xl text-blue">
+            <img src="/images/recursos miguitas-03.png" alt="" />
+        </a>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "PawtyPacks",
+};
+</script>
