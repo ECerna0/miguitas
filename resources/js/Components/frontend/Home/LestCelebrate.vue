0d324ea189e4958eae1ed6e80a29cb737e782d2f
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-08 21:52:15 -0600

    feat: add section lest celebrate

---
+++ resources/js/Components/frontend/Home/LestCelebrate.vue
@@ -0,0 +1,13 @@
+<template>
+    <section
+        class="flex items-center mx-auto rounded-3xl w-4/5 overflow-hidden"
+    >
+        <img src="/images/celebrate.png" alt="" />
+    </section>
+</template>
+
+<script>
+export default {
+    name: "LestCelebrate",
+};
+</script>
