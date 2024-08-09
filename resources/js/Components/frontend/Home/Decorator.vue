f9275601f81a1a4a8d2dc6782eda2bc78359cc6d
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-08 21:51:19 -0600

    feat: add section decorator

---
+++ resources/js/Components/frontend/Home/Decorator.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="flex items-center gap-28 bg-lightBlue py-8">
+        <div class="flex flex-col flex-1 items-end gap-2">
+            <h3 class="w-2/3 font-pluto_m text-4xl text-center text-lightGray">
+                Más que un TREAT, una muestra de amor y cuidado
+            </h3>
+            <p class="w-2/3 font-pluto_m text-center text-lg text-pink">
+                Queremos ser parte de su vida
+            </p>
+        </div>
+        <div class="flex flex-1 justify-start">
+            <img
+                src="/images/perritocumple.png"
+                alt="Perrito cumple"
+                class="rounded-3xl w-96"
+            />
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "Decorator",
+};
+</script>
