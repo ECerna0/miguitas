c177f1399b7f641757b4a1130cd563fc5ee76d52
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-13 17:34:07 -0600

    feat: add component footer head

---
+++ resources/js/Components/frontend/Footer/FootHead.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Logo from "@Frontend/Logo.vue";
+</script>
+
+<template>
+    <div class="flex items-center gap-12">
+        <div>
+            <Logo />
+        </div>
+        <div class="flex flex-col items-center gap-2">
+            <h1 class="font-pluto_m text-4xl text-pink uppercase">Miguitas</h1>
+            <p class="font-dine_r text-center text-sm text-white">
+                Horneamos todos los días para los consentidos de cuatro patitas.
+                ¡Elaboramos pasteles, pupcakes y galletas de diferentes sabores,
+                tamaño y forma para que cada día puedas consentir a tu perrito o
+                gatito de la mejor manera: natural!
+            </p>
+        </div>
+    </div>
+</template>
