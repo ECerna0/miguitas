d636624401c1c7fbaeba39506c6b26d983f25e75
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-13 17:35:00 -0600

    feat: add the components that structure the footer

--- resources/js/Components/frontend/Footer/Footer.vue
+++ resources/js/Components/frontend/Footer/Footer.vue
@@ -1,23 +1,15 @@
+<script setup>
+import FootHead from "@Frontend/Footer/FootHead.vue";
+import FootBody from "@Frontend/Footer/FootBody.vue";
+import FootFot from "@Frontend/Footer/FootFot.vue";
+</script>
+
 <template>
-    <footer class="bg-blue p-8">
-        <div class="mx-auto w-4/5">
-            <div class="flex items-center gap-12">
-                <img src="/images/logo.png" alt="" class="w-36 h-36" />
-                <div class="flex flex-col items-center gap-2">
-                    <h1 class="font-pluto_m text-4xl text-pink uppercase">
-                        Miguitas
-                    </h1>
-                    <p class="w-4/5 font-dine_r text-center text-white">
-                        Horneamos todos los días para los consentidos de cuatro
-                        patitas. ¡Elaboramos pasteles, pupcakes y galletas de
-                        diferentes sabores, tamaño y forma para que cada día
-                        puedas consentir a tu perrito o gatito de la mejor
-                        manera: natural!
-                    </p>
-                </div>
-            </div>
-            <div></div>
-            <div></div>
+    <footer class="bg-blue">
+        <div class="mx-auto p-8 w-[70%]">
+            <FootHead />
+            <FootBody />
         </div>
+        <FootFot />
     </footer>
 </template>
