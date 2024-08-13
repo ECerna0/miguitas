19c3083b5bdb0c535613f7b77fc0864fbb59ebf9
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-13 17:31:28 -0600

    feat: add logo component

---
+++ resources/js/Components/frontend/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-24 h-24">
+        <img
+            src="/images/logo.png"
+            alt="Logo image"
+            class="w-full h-full object-cover"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavbarLogo",
+};
+</script>
