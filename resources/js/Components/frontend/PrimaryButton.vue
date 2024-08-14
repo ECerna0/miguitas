136b4db037258fe3176cb29dd223782e81e21e75
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-13 22:14:57 -0600

    refactor: update script configuration to septup and add const props and const classes

--- resources/js/Components/frontend/PrimaryButton.vue
+++ resources/js/Components/frontend/PrimaryButton.vue
@@ -4,33 +4,31 @@
     </button>
 </template>
 
-<script>
-export default {
-    name: "PrimaryButton",
-    props: {
-        buttonText: {
-            type: String,
-            default: "Button",
-        },
-        class: {
-            type: String,
-            default: "",
-        },
-        isDisabled: {
-            type: Boolean,
-            default: false,
-        },
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+    buttonText: {
+        type: String,
+        default: "Button",
     },
-    computed: {
-        classes() {
-            const baseClasses =
-                "flex justify-center items-center px-6 pt-3 pb-2 rounded-full font-bold font-pluto_m text-xl uppercase transition-colors";
-            const enabledClasses = "bg-blue hover:bg-blueSelected text-white";
-            const disabledClasses = "bg-gray text-white cursor-not-allowed";
-            return `${baseClasses} ${
-                this.isDisabled ? disabledClasses : enabledClasses
-            } ${this.additionalClasses}`;
-        },
+    class: {
+        type: String,
+        default: "",
     },
-};
+    isDisabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const classes = computed(() => {
+    const baseClasses =
+        "flex justify-center items-center px-6 pt-3 pb-2 rounded-full font-bold font-pluto_m text-xl uppercase transition-colors";
+    const enabledClasses = "bg-blue hover:bg-blueSelected text-white";
+    const disabledClasses = "bg-gray text-white cursor-not-allowed";
+    return `${baseClasses} ${
+        props.isDisabled ? disabledClasses : enabledClasses
+    } ${props.class}`;
+});
 </script>
