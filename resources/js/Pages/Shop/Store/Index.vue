ab9755613bbd41a54932d032a5cfa0468969d46c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-14 17:10:28 -0600

    feat: add components to the store page

---
+++ resources/js/Pages/Shop/Store/Index.vue
@@ -0,0 +1,18 @@
+<script setup>
+import UserLayout from "@/Layouts/UserLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+import StoreHeader from "@Frontend/Store/StoreHeader.vue";
+import Categories from "@Frontend/Store/Categories.vue";
+import FeaturedProducts from "@Frontend/Store/FeaturedProducts.vue";
+import InfoContainer from "@Frontend/Store/InfoContainer.vue";
+</script>
+
+<template>
+    <UserLayout>
+        <Head title="Tienda" />
+        <StoreHeader />
+        <Categories />
+        <FeaturedProducts />
+        <InfoContainer />
+    </UserLayout>
+</template>
