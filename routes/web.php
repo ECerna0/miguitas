d886f22db402300e04300a535ab37cd2a65e5643
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-14 17:24:52 -0600

    feat: add route shop

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     ]); */
 });
 
+Route::get("/shop", function () {
+    return Inertia::render("Shop/Store/Index");
+})->name("shop.index");
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
